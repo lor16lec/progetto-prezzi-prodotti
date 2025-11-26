b30a66d49b392e2823376d2cdaa9a9cc8e99e707
BOSSI.21100 <BOSSI.21100@LABI1-13PC04>
2025-11-26 13:57:03 +0100

    progetto finito

--- grafico/Form1.Designer.cs
+++ grafico/Form1.Designer.cs
@@ -31,17 +31,26 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,7 +60,7 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(336, 49);
+            this.chart1.Location = new System.Drawing.Point(954, 37);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
@@ -61,22 +70,6 @@
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
-            // chart2
-            // 
-            chartArea2.Name = "ChartArea1";
-            this.chart2.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart2.Legends.Add(legend2);
-            this.chart2.Location = new System.Drawing.Point(314, 187);
-            this.chart2.Name = "chart2";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart2.Series.Add(series2);
-            this.chart2.Size = new System.Drawing.Size(65, 9);
-            this.chart2.TabIndex = 1;
-            this.chart2.Text = "chart2";
-            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -84,7 +77,7 @@
             this.Column1,
             this.Column2,
             this.Column3});
-            this.dataGridView1.Location = new System.Drawing.Point(13, 37);
+            this.dataGridView1.Location = new System.Drawing.Point(678, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(245, 401);
             this.dataGridView1.TabIndex = 2;
@@ -104,32 +97,172 @@
             this.Column3.HeaderText = "Column3";
             this.Column3.Name = "Column3";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(227, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 153);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(227, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 210);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(227, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 270);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(227, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 324);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(227, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 299);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 106);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(699, 473);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "label7";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1451, 760);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.chart2);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- grafico/Form1.cs
+++ grafico/Form1.cs
@@ -10,71 +10,137 @@ namespace grafico
         public Form1()
         {
             InitializeComponent();
+
+            // Registrazione evento TextChanged (opzionale)
+            textBox1.TextChanged += TextBoxes_TextChanged;
+            textBox2.TextChanged += TextBoxes_TextChanged;
+            textBox3.TextChanged += TextBoxes_TextChanged;
+            textBox4.TextChanged += TextBoxes_TextChanged;
+            textBox5.TextChanged += TextBoxes_TextChanged;
+            textBox6.TextChanged += TextBoxes_TextChanged;
+
+            // All'avvio nascondi chart e table
+            chart1.Visible = false;
+            dataGridView1.Visible = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // ----------------------------
-            // PARAMETRI CURVA DOMANDA
-            // ----------------------------
-            double B2 = 90;   // intercetta domanda
-            double B3 = 4;    // coefficiente domanda
+            // Nessun valore di default
+        }
 
-            // ----------------------------
-            // PARAMETRI CURVA OFFERTA
-            // ----------------------------
-            double C1 = 10;     // intercetta offerta
-            double C2 = 0.01;   // coefficiente offerta
-            double C3 = 3;      // esponente offerta
+       
 
-            // ----------------------------
-            // CONFIGURAZIONE GRAFICO
-            // ----------------------------
+        private void TextBoxes_TextChanged(object sender, EventArgs e)
+        {
+            // Potresti voler non aggiornare alla digitazione
+            // oppure tenerlo disattivato se preferisci solo con il bottone
+            // AggiornaGraficoETabella();
+        }
+
+        private void AggiornaGraficoETabella()
+        {
+            if (!double.TryParse(textBox1.Text, out double B2)) return;
+            if (!double.TryParse(textBox2.Text, out double B3)) return;
+            if (!double.TryParse(textBox3.Text, out double C1)) return;
+            if (!double.TryParse(textBox4.Text, out double C2)) return;
+            if (!double.TryParse(textBox5.Text, out double C3)) return;
+            if (!int.TryParse(textBox6.Text, out int Qmax) || Qmax < 0) return;
+
+            // 2. Mostra grafico e tabella
+            chart1.Visible = true;
+            dataGridView1.Visible = true;
+
+            // 3. Pulisci grafico e inizializza
             chart1.Series.Clear();
-            chart1.ChartAreas[0].AxisX.Title = "Quantità";
-            chart1.ChartAreas[0].AxisY.Title = "Prezzo";
             chart1.Titles.Clear();
             chart1.Titles.Add("Domanda e Offerta");
+            chart1.ChartAreas[0].AxisX.Title = "Quantità (q)";
+            chart1.ChartAreas[0].AxisY.Title = "Prezzo";
 
-            Series domanda = new Series("Domanda");
-            domanda.ChartType = SeriesChartType.Line;
-            domanda.Color = Color.Green;
-            domanda.BorderWidth = 3;
-
-            Series offerta = new Series("Offerta");
-            offerta.ChartType = SeriesChartType.Line;
-            offerta.Color = Color.Blue;
-            offerta.BorderWidth = 3;
-
+            Series domanda = new Series("Domanda")
+            {
+                ChartType = SeriesChartType.Line,
+                Color = Color.Green,
+                BorderWidth = 2
+            };
+            Series offerta = new Series("Offerta")
+            {
+                ChartType = SeriesChartType.Line,
+                Color = Color.Blue,
+                BorderWidth = 2
+            };
             chart1.Series.Add(domanda);
             chart1.Series.Add(offerta);
 
-            // ----------------------------
-            // CONFIGURAZIONE TABELLA
-            // ----------------------------
+            // 4. Inizializza la tabella
             dataGridView1.Columns.Clear();
             dataGridView1.Rows.Clear();
-
-            dataGridView1.Columns.Add("q", "Quantità");
+            dataGridView1.Columns.Add("q", "q");
             dataGridView1.Columns.Add("d", "Domanda");
             dataGridView1.Columns.Add("o", "Offerta");
 
-            // ----------------------------
-            // CALCOLO VALORI 0 → 20
-            // ----------------------------
-            for (int q = 0; q <= 20; q++)
+            // 5. Popola grafico e tabella per q interi
+            for (int q = 0; q <= Qmax; q++)
             {
-                double d = B2 - (B3 * q);                  // domanda
-                double o = C1 + (C2 * Math.Pow(q, C3));    // offerta
+                double d = B2 - (B3 * q);
+                double o = C1 + (Math.Pow(q, C3) / C2);
 
-                // Aggiunta punti al grafico
                 domanda.Points.AddXY(q, d);
                 offerta.Points.AddXY(q, o);
 
-
-                // Aggiunta alla tabella
                 dataGridView1.Rows.Add(q, d.ToString("0.00"), o.ToString("0.00"));
             }
+
+            // 6. Calcola approssimazione del punto di equilibrio (continua, passo fine)
+            double bestQ = 0;
+            double bestDiff = double.MaxValue;
+            double priceAtEq = 0;
+            double step = 0.1;  // passo per maggiore precisione
+
+            for (double q = 0.0; q <= Qmax; q += step)
+            {
+                double d = B2 - (B3 * q);
+                double o = C1 + (Math.Pow(q, C3) / C2);
+                double diff = Math.Abs(d - o);
+                if (diff < bestDiff)
+                {
+                    bestDiff = diff;
+                    bestQ = q;
+                    priceAtEq = (d + o) / 2.0;
+                }
+            }
+
+            // 7. Se la differenza minima è ragionevole, segna punto di equilibrio
+            // Alla fine del metodo AggiornaGraficoETabella(), dopo aver calcolato bestQ, priceAtEq, bestDiff:
+            double soglia = 0.5;  // come prima
+            if (bestDiff < soglia)
+            {
+                // Segna il punto sul grafico
+                Series eq = new Series("Equilibrio")
+                {
+                    ChartType = SeriesChartType.Point,
+                    MarkerStyle = MarkerStyle.Circle,
+                    MarkerSize = 10,
+                    Color = Color.Red
+                };
+                chart1.Series.Add(eq);
+                eq.Points.AddXY(bestQ, priceAtEq);
+
+                // Aggiorna la label con i valori di equilibrio
+                label7.Text = $"Equilibrio ≃ q = {bestQ:F2}, p = {priceAtEq:F2}";
+            }
+            else
+            {
+                // Se non trova un buon equilibrio, svuota o informa
+                label7.Text = "Nessun equilibrio trovato (auto ≠ offerta)";
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AggiornaGraficoETabella();
         }
     }
 }
